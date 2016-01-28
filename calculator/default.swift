85dcca66c5f762b56738886c9445b236709e4a63
nsafai <nicolaisafai@gmail.com>
2016-01-28 01:43:58 -0800

    v1 finished for iPhone6 and Iphone6S

--- calculator/default.swift
+++ calculator/default.swift
@@ -15,6 +15,8 @@ class TipViewController: UIViewController {
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var whiteView: UIView!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +28,8 @@ class TipViewController: UIViewController {
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         let defaults = NSUserDefaults.standardUserDefaults()
-        tipControl.selectedSegmentIndex = defaults.integerForKey("default_tip_index")
+        tipControl.selectedSegmentIndex = defaults.integerForKey("default_tip_index") // visually change selected index if default has changed in defaults
+        onEdit(self) // update the tip amount if default tip % has been changed
     }
 
     override func didReceiveMemoryWarning() {
@@ -42,18 +45,22 @@ class TipViewController: UIViewController {
 
         if let billAmount = Double(billAmountField.text!) {
 //            print(billAmount)
+            
             let tipAmount = billAmount*chosenTip
             let totalAmount = billAmount+tipAmount
             
             tipLabel.text = String(format:"$%.2f", tipAmount)
             totalLabel.text = String(format:"$%.2f", totalAmount)
+//            topView.center.x = topView.center.x+100
+            billAmountField.center.y = billAmountField.center.y+100
+            
         } else {
             tipLabel.text = ""
             totalLabel.text = ""
         }
     }
     @IBAction func onTap(sender: AnyObject) {
-        view.endEditing(true)
+//        view.endEditing(true)
     }
 }
 

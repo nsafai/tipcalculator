b73376c710d70b4af11a68e83b6d69e073c9c159
nsafai <nicolaisafai@gmail.com>
2016-01-28 00:15:25 -0800

    functional calculator

---
+++ calculator/SettingsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SettingsViewController.swift
+//  calculator
+//
+//  Created by Nicolai Safai on 1/27/16.
+//  Copyright © 2016 Lime. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+    }
+    
+
+}
---
+++ calculator/TipViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Nicolai Safai on 1/23/16.
+//  Copyright © 2016 Lime. All rights reserved.
+//
+
+import UIKit
+
+class TipViewController: UIViewController {
+
+
+    @IBOutlet weak var billAmountField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        billAmountField.becomeFirstResponder()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func onEdit(sender: AnyObject) {
+        
+        var tipPercentages =  [0.1, 0.15, 0.2]
+        let chosenTip = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        if let billAmount = Double(billAmountField.text!) {
+//            print(billAmount)
+            let tipAmount = billAmount*chosenTip
+            let totalAmount = billAmount+tipAmount
+            
+            tipLabel.text = String(format:"$%.2f", tipAmount)
+            totalLabel.text = String(format:"$%.2f", totalAmount)
+        } else {
+            tipLabel.text = ""
+            totalLabel.text = ""
+        }
+    }
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+}
+
